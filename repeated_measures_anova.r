8814962b7a6b138025b7f1416b96552ea9b444dd
davidjpmoore <davidjpmoore@email.arizona.edu>
2015-04-06 11:30:30 -0700

    updated to include some notes about statistic models and tests

--- repeated_measures_anova.r
+++ repeated_measures_anova.r
@@ -2,6 +2,7 @@
 #cheatsheet - http://www.rstudio.com/wp-content/uploads/2015/02/data-wrangling-cheatsheet.pdf
 library(dplyr)
 
+#read data in
 aci <- read.table (file="data/2013_2014_photosynthesis_master.csv", sep=",", header=TRUE)
 
 #display table of data
@@ -10,12 +11,9 @@ tbl_df(aci)
 #ISOdate or rDate formats
 aci$rDate = as.Date(aci$date, "%m/%d/%Y")
 
-
 #subsetting data for 2013 and 2014 by themselves
 aci2013 <- subset(aci, year == 2013)
 aci2014 <- subset(aci, year == 2014)
-
-plot(aci$date,aci$vcmax)
 
 #do some simple plots
 
@@ -67,6 +65,29 @@ a14 + theme_bw() + aes(shape = factor(genotype)) +
   geom_point(colour="grey90", size = 1.5)
 
 
+
+#examine simple plots and write down step by step what you need to make the next plot
+
+# 1) we want a plot of the MEAN Vcmax for EACH genotype at each date
+# 2) we want the standard error of that mean
+# could use tapply
+
+# 3) we want to know if these means are different from each other
+#could do an ANOVA - repeated measures ANOVA 
+
+# Does my data conform to the ASSUMPTIONS OF THE ANALYSIS I WANT TO DO?
+# What are the assumptions of the ANOVA?
+
+# QUESTION1: What is the appropriate model structure for THIS analysis? (what are the main effects, interactions - what is your unit of replication?)
+# QUESTION1: WHAT are the differences I care about for THIS analysis?
+# Question3: What is the appropriate statistical test? 
+
+
+######
+#try UCLA webpage for examples of statistical models
+
+demo1.aov <- aov(vcmax ~ genotype * rdate + Error(tree), data = aci)
+summary(demo1.aov)
 
 
 aov.out = aov(vcmax ~ rDate + Error(tree/rDate), data=aci)
