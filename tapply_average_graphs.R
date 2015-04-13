ba685efb1149bae776fc14fdd01b55b92678eb5f
ianshiach <shiach@email.arizona.edu>
2015-04-13 10:54:11 -0700

    Added bar graphs

--- tapply_average_graphs.R
+++ tapply_average_graphs.R
@@ -1,6 +1,12 @@
 setwd("C:/Users/ianshiach/Desktop/Grad School/Thesis/Processing/R/Processing")
 aci <- read.table (file="2013_2014_photosynthesis_master.csv", sep=",", header=TRUE)
 
+#purpose
+#author
+
+#libraries
+library(ggplot2)
+
 #subset 2014
 aci2014 <- subset(aci, year == 2014)
   
@@ -17,17 +23,17 @@ summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
                       conf.interval=.95, .drop=TRUE) {
   library(plyr)
   
-  # New version of length which can handle NA's: if na.rm==T, don't count them
-  length2 <- function (x, na.rm=FALSE) {
+  # New version of vcmaxgth which can handle NA's: if na.rm==T, don't count them
+  vcmaxgth2 <- function (x, na.rm=FALSE) {
     if (na.rm) sum(!is.na(x))
-    else       length(x)
+    else       vcmaxgth(x)
   }
   
   # This does the summary. For each group's data frame, return a vector with
   # N, mean, and sd
   datac <- ddply(data, groupvars, .drop=.drop,
                  .fun = function(xx, col) {
-                   c(N    = length2(xx[[col]], na.rm=na.rm),
+                   c(N    = vcmaxgth2(xx[[col]], na.rm=na.rm),
                      mean = mean   (xx[[col]], na.rm=na.rm),
                      sd   = sd     (xx[[col]], na.rm=na.rm)
                    )
@@ -56,8 +62,9 @@ summary
 #plot means
 
 
+
 # Standard error of the mean
-ggplot(aci2014, aes(x=month, y=vcmax, colour=supp)) + 
+ggplot(summary, aes(x=month, y=vcmax, colour=state)) + 
   geom_errorbar(aes(ymin=vcmax-se, ymax=vcmax+se), width=.1) +
   geom_line() +
   geom_point()
@@ -66,21 +73,41 @@ ggplot(aci2014, aes(x=month, y=vcmax, colour=supp)) +
 # The errorbars overlapped, so use position_dodge to move them horizontally
 pd <- position_dodge(0.1) # move them .05 to the left and right
 
-ggplot(aci2014, aes(x=month, y=vcmax, colour=supp)) + 
+ggplot(summary, aes(x=month, y=vcmax, colour=state)) + 
   geom_errorbar(aes(ymin=vcmax-se, ymax=vcmax+se), width=.1, position=pd) +
   geom_line(position=pd) +
   geom_point(position=pd)
 
 
 # Use 95% confidence interval instead of SEM
-ggplot(aci2014, aes(x=month, y=vcmax, colour=supp)) + 
+ggplot(summary, aes(x=month, y=vcmax, colour=state)) + 
   geom_errorbar(aes(ymin=vcmax-ci, ymax=vcmax+ci), width=.1, position=pd) +
   geom_line(position=pd) +
   geom_point(position=pd)
 
-# Black error bars - notice the mapping of 'group=supp' -- without it, the error
+# Black error bars - notice the mapping of 'group=state' -- without it, the error
 # bars won't be dodged!
-ggplot(aci2014, aes(x=month, y=vcmax, colour=supp, group=supp)) + 
+ggplot(summary, aes(x=month, y=vcmax, colour=state, group=state)) + 
   geom_errorbar(aes(ymin=vcmax-ci, ymax=vcmax+ci), colour="black", width=.1, position=pd) +
   geom_line(position=pd) +
   geom_point(position=pd, size=3)
+
+
+# Use month as a factor rather than numeric
+summary2 <- summary
+summary2$month <- factor(summary2$month)
+
+# Error bars represent standard error of the mean
+ggplot(summary2, aes(x=month, y=vcmax, fill=state)) + 
+  geom_bar(position=position_dodge(), stat="identity") +
+  geom_errorbar(aes(ymin=vcmax-se, ymax=vcmax+se),
+                width=.2,                    # Width of the error bars
+                position=position_dodge(.9))
+
+
+# Use 95% confidence intervals instead of SEM
+ggplot(summary2, aes(x=month, y=vcmax, fill=state)) + 
+  geom_bar(position=position_dodge(), stat="identity") +
+  geom_errorbar(aes(ymin=vcmax-ci, ymax=vcmax+ci),
+                width=.2,                    # Width of the error bars
+                position=position_dodge(.9))
