224820f8427b2bd22e4cfe092475f43d52e36866
ianshiach <shiach@email.arizona.edu>
2015-04-13 10:33:10 -0700

    Fixed replace error

--- tapply_average_graphs.R
+++ tapply_average_graphs.R
@@ -17,17 +17,17 @@ summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
                       conf.interval=.95, .drop=TRUE) {
   library(plyr)
   
-  # New version of vcmaxgth which can handle NA's: if na.rm==T, don't count them
-  vcmaxgth2 <- function (x, na.rm=FALSE) {
+  # New version of length which can handle NA's: if na.rm==T, don't count them
+  length2 <- function (x, na.rm=FALSE) {
     if (na.rm) sum(!is.na(x))
-    else       vcmaxgth(x)
+    else       length(x)
   }
   
   # This does the summary. For each group's data frame, return a vector with
   # N, mean, and sd
   datac <- ddply(data, groupvars, .drop=.drop,
                  .fun = function(xx, col) {
-                   c(N    = vcmaxgth2(xx[[col]], na.rm=na.rm),
+                   c(N    = length2(xx[[col]], na.rm=na.rm),
                      mean = mean   (xx[[col]], na.rm=na.rm),
                      sd   = sd     (xx[[col]], na.rm=na.rm)
                    )
