c736a2f0011430c2487d0c26d836a61920039ffb
David Moore <davidjpmoore@email.arizona.edu>
2015-08-27 17:00:31 -0700

    Updates to met data processing for Amberly's Isoprene manuscript
    Met data processing updated to calculate simple weekly sums

--- rDate scraps.R
+++ rDate scraps.R
@@ -5,6 +5,9 @@
 
 # example as.Date('1/15/2001',format='%m/%d/%Y')
 
+
+#Is you have date the format MM/DD/YYYY
+
 #DF means DATAFRAME
 DF$rDate = as.Date(DF$LICORDATE, "%m/%d/%Y")
 #note you could just overright
@@ -14,3 +17,12 @@ DF$rDate = as.Date(DF$LICORDATE, "%m/%d/%Y")
 
 DF$MONTH = as.numeric(strftime(DF$rDate, format = "%m"))
 DF$WEEK = as.numeric(strftime(DF$rDate, format = "%W"))
+
+#to create a date variable
+
+DF$YEAR=2015
+DF$MONTH=6
+DF$DAY=15
+
+DF$rDate  = as.Date(ISOdate(DF$YEAR,DF$MONTH,DF$DAY))
+
