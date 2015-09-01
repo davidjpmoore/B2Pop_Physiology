0497707e325bfa6a3d814b3fbef27e778dfc8a0c
ianshiach <shiach@email.arizona.edu>
2015-08-31 21:38:41 -0700

    Added message about average .csv

--- isoprene_calibration.R
+++ isoprene_calibration.R
@@ -3,6 +3,8 @@ setwd("C:/Users/ianshiach/Desktop/Grad School/Thesis/Data/Isoprene/")
 # read in calibration data
 iso_cal <- read.table (file="06262015_isoprene_calibration.csv", sep=",", header=TRUE)
 
+
+#
 # Calculate average CPS for each calibration and FDPS value/known isoprene value
 avg_cal <- data.frame(with(iso_cal, tapply(cps, list(calibration_number, known_isoprene), mean)))
 
