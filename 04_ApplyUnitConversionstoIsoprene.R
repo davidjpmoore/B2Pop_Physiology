3fd965d942db43f279a15d0daba7604c9c4c9405
David Moore <davidjpmoore@email.arizona.edu>
2015-09-01 16:59:50 -0700

    file template for unit conversions

---
+++ 04_ApplyUnitConversionstoIsoprene.R
@@ -0,0 +1,23 @@
+### Author: Ian Shiach
+### Date: 09/01/2015
+### Purpose: To calculate a flux of isoprene in mmol isoprene/m2/sec
+
+#define constants
+
+ug_to_g_iso= 1/1000000
+
+#source 
+#units can be put in the name?
+DensAir_at35C = 1.1455/1
+
+mol_per_g = 
+
+#read isoprene in ppbv
+#load df iso_ppbv 
+
+  
+#apply unit converions
+  isoprene_ppbv$isoprene_mmol_perm2_persec = some math
+
+  
+
