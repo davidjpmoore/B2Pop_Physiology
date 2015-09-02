629fbde104652a772fe206f26cf873b740e1ec30
ianshiach <shiach@email.arizona.edu>
2015-09-02 15:08:46 -0700

    Added unit conversion factors and equation for converting units, and appended mmol_isoprene/(s*m^2) values into new column in data frame

--- 04_ApplyUnitConversionstoIsoprene.R
+++ 04_ApplyUnitConversionstoIsoprene.R
@@ -1,23 +1,84 @@
 ### Author: Ian Shiach
 ### Date: 09/01/2015
-### Purpose: To calculate a flux of isoprene in mmol isoprene/m2/sec
+### Purpose: To calculate a flux of isoprene in mmol isoprene/m2/sec from ppbv isoprene
+
+#read isoprene in ppbv
+#load df isoprene_ppbv 
+
+load("isoprene_ppbv.Rda")
+
+# the input in ppbv can also be expressed as ug_isoprene/kg_air
 
 #define constants
 
-ug_to_g_iso= 1/1000000
+##############################
+# micrograms of isoprene to milimoles of isoprene (ug_iso -> mmol_iso)
 
-#source 
-#units can be put in the name?
-DensAir_at35C = 1.1455/1
+# micrograms of isoprene to grams of isoprene
+# 1 g_iso/1000000 ug_iso
 
-mol_per_g = 
+a <- 1/1000000
 
-#read isoprene in ppbv
-#load df iso_ppbv 
+# grams of isoprene to moles of isoprene
+# 1 mol_iso/68.12 g_iso
 
-  
+b <- 1/68.12
+
+# moles of isoprene to milimoles of isoprene
+# 1000 mmol_iso/1 mol_iso
+
+c <- 1000/1
+
+# looks like it should be nanomoles of isoprene
+
+###############################
+# kilograms of air to seconds (kg_air -> s)
+
+# kilograms of air to cubic meters of air (density of air;  temperature dependent)
+# need to add source of value and possibly change to equation
+# this is the value for 35C
+# 1.1455 kg_air/1 m^3_air
+
+d <- 1.1455/1
+
+# cubic meters of air to liters of air
+# 1 m^3_air/1000 L_air
+
+e <- 1/1000
+
+# liters of air to moles of air
+# 22.4 L_air/1 mol_air
+
+f <- 22.4/1
+
+# moles of air to micromoles of air
+# 1 mol_air/1000000 umol_air
+
+g <- 1/1000000
+
+# micromoles of air to seconds (LICOR flow rate)
+# 600 umol_air/1 s
+
+h <- 600/1
+
+###############################
+# milimoles of isoprene per second to milimoles of isoprene per square meter second (mmol_iso/s -> mmol_iso/(s*m^2))
+
+# divide by leaf area (area of LICOR cuvette head)
+# 1/6 cm^2
+
+i <- 1/6
+
+# square centimeters to square meters
+# 10000 cm^2/1 m^2
+
+j <- 10000/1
+
+###############################
 #apply unit converions
-  isoprene_ppbv$isoprene_mmol_perm2_persec = some math
+
+
+isoprene_ppbv$isoprene_mmol_per_m2_s <- isoprene_ppbv$isoprene_ppbv * a * b * c * d * e * f * g * h * i * j
 
   
 
