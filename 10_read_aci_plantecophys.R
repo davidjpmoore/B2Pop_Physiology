ab12e4d602574ae6f4651c4db4990ca7eaac420b
ianshiach <shiach@email.arizona.edu>
2015-09-17 15:25:57 -0700

    Initial commit

---
+++ 10_read_aci_plantecophys.R
@@ -0,0 +1,76 @@
+#00_testingPlantEcoPhys
+#Author: Dave Moore
+#Date: 09/05/2015
+#Purpose: Test the Plantecophys package - a stand alone package to model common leaf gas exchange measurements
+
+
+# Using fitaci [library (plantecophys)] to estimate Vcmax and Jmax
+#
+# Note: you need to specify the dataframe and the variables that correspond to ALEAF, Tleaf, Ci and PPFD
+# Note: I haven't worked out how to exclude outliers in any sensible way 
+#    but PECAN:Photosynthesis has this function built in - perhaps I can use them together?
+
+# download the source code for the package: https://cran.r-project.org/web/packages/plantecophys/index.html
+#install.packages('PATH/plantecophys_0.6-3.zip', repos = NULL, type="source")
+#Manual: https://cran.r-project.org/web/packages/plantecophys/plantecophys.pdf 
+
+# updated R 09/14/2015
+# updated Rstudio 09/14/2015
+# updated dplyr 09/14/2015
+# updated plantecophys to developer version 0.6.6
+# library(devtools)
+# install_bitbucket("remkoduursma/plantecophys")
+
+library(devtools)
+library (plantecophys)
+library(dplyr)
+library(ggplot2)
+library(grid) #required for 'unit'
+#Load data
+#Amberly's data from B2
+
+#loading dat_Iso1.Rda - isoprene unit conversions completed using 00_ReadIsoprene_UnitConversions
+load ("pita_06082014.Rda")
+
+#to make this from scratch just run: 00_ReadIsoprene_UnitConverions.R 
+
+##########################################
+#Use filter, mutate and select in one step
+##########################################
+
+# For the simple case - create a dataframe that is a subset of dat_Iso_01.Rda containing a single
+# A/ci curve - I picked the line and data at random from a list of available lines and dates.
+#
+Junkaci_001 = pita_06082014   %>% #piping command for filter
+  #restrict to a single Aci curve filter out any Anet values that are too big
+  #filter(Anet < 50) %>% #piping command for mutate
+  #create new variables (rename them) to be consistent with fitacis
+  #Grouping by genetic line, measurement date and reference temperature
+  mutate(ACIgroups=as.factor(paste(Tleaf, sep="")), PARi=PARi, dateMeas="06/08/2014", CO2S=CO2S, Ci=Ci, Tleaf=Tleaf, Photo=Photo, Patm=Press)  %>%
+  
+  #select only these variables
+  select(dateMeas,ACIgroups, CO2S,Ci,Tleaf,Photo, PARi, Patm)  
+
+#group by ACIgroups
+Junkaci_002 = Junkaci_001 %>%
+  group_by(ACIgroups)
+
+
+CheckACI= fitacis(Junkaci_002, "ACIgroups")
+
+
+ACi <- ggplot(Junkaci_002, aes(x=Ci, y=Photo))
+# 
+ACi + aes(shape = factor(Tleaf)) +
+  ggtitle(paste("Group", ACIgroups, sep=" "))+
+  geom_point(aes(colour = factor(Tleaf)), size = 8) +
+  #geom_point(aes(group = ACIgroups))+
+  facet_wrap(~ ACIgroups) +
+  geom_point(colour="grey90", size = 2.5) +
+  theme_classic() +
+  theme(axis.text=element_text(size=20),
+        axis.title=element_text(size=22,face="bold")) + 
+  theme(panel.border = element_blank(), axis.line = element_line(colour="black", size=2, lineend="square"))+
+  theme(axis.ticks = element_line(colour="black", size=2, lineend="square"))+
+  ylab("Assimilation (umol/m2/sec)")+
+  xlab("Ci") 
