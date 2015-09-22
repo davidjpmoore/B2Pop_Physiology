4ea9894e21944ff5e4c21e6b171e87392e3c36c3
ianshiach <shiach@email.arizona.edu>
2015-09-21 21:15:58 -0700

    Added code to create new data frame for each tree

--- 09_separate_files_for_all_trees.R
+++ 09_separate_files_for_all_trees.R
@@ -17,7 +17,7 @@ flux_09092014 <- read.Licor("2014_aci/09-09-2014-ims-aci-flux")
 leila_09092014 <- read.Licor("2014_aci/09-09-2014-ims-aci-leila")
 pita_09092014 <- read.Licor("2014_aci/09-09-2014-ims-aci-pita")
 
-# Change "fname" to date-LICOR name-tree-line
+# Change "fname" values to date-(LICOR name)-tree-line
 
 pita_06082014$fname <- "06/08/2014-pita-tree-line"
 princess_06082014$fname <- "06/08/2014-princess-tree-line"
@@ -27,7 +27,45 @@ flux_09092014$fname <- "06/08/2014-flux-tree-line"
 leila_09092014$fname <- "06/08/2014-leila-tree-line"
 pita_09092014$fname <- "06/08/2014-pita-tree-line"
 
-# Create new data frame for each tree
+# Create new data frame for each tree/date
+# June Pita
+
+june_a10 <- pita_06082014[14:26, ]
+june_a19 <- pita_06082014[27:39, ]
+june_a13 <- pita_06082014[66:78, ]
+june_b15 <- pita_06082014[79:91, ]
+june_b13 <- pita_06082014[92:104, ]
+june_e10 <- pita_06082014[105:117, ]
+june_c17 <- pita_06082014[118:130, ]
+june_f16 <- pita_06082014[131:143, ]
+june_c02 <- pita_06082014[157:169, ]
+
+# June Princess
+
+june_b12 <- princess_06082014[1:13, ]
+june_h08 <- princess_06082014[14:26, ]
+june_a03 <- princess_06082014[27:39, ]
+june_c03 <- princess_06082014[40:52, ]
+june_d10 <- princess_06082014[53:65, ]
+june_g10 <- princess_06082014[66:78, ]
+june_e03 <- princess_06082014[79:91, ]
+june_d14 <- princess_06082014[92:104, ]
+june_e14 <- princess_06082014[105:117, ]
+june_c19 <- princess_06082014[118:130, ]
+june_e18 <- princess_06082014[131:143, ]
+june_f19 <- princess_06082014[144:156, ]
+june_d17 <- princess_06082014[157:169, ]
+june_h09 <- princess_06082014[170:182, ]
+june_g06 <- princess_06082014[183:195, ]
+june_d06 <- princess_06082014[196:208, ]
+
+
+
+
+
+
+# Reset "Obs" 
+
 
 # Change "fname" to reflect tree and line
 
