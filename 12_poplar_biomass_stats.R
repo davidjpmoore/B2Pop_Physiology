198dececec6850f1934bcca9f2be16cb193e0505
ianshiach <shiach@email.arizona.edu>
2015-11-04 16:26:53 -0700

    Added code to draw bar charts

--- 12_poplar_biomass_stats.R
+++ 12_poplar_biomass_stats.R
@@ -6,11 +6,12 @@
 
 
 library(plyr)
-library(car)
+#library(car)
 library(ggplot2)
-library(agricolae)
-library(multcomp)
-library(nlme)
+library(lsmeans)
+#library(agricolae)
+#library(multcomp)
+#library(nlme)
 
 
 setwd("./data/")
@@ -75,6 +76,119 @@ a <- aov$`(Intercept)`
 b <- aov$ tree
 c <- aov$`tree:yr`
 a$residuals
+
+
+# Least squares means from https://cran.r-project.org/web/packages/lsmeans/vignettes/using-lsmeans.pdf
+
+
+
+biomass.lm1 <- lm(biomass ~ yr + state, data = biomass_for_anova2)
+anova(biomass.lm1)
+
+# define reference grid
+
+biomass.rg1 <- ref.grid(biomass.lm1)
+summary(biomass.rg1)
+
+# 
+
+lsmeans(biomass.lm1, "yr")
+
+lsmeans(biomass.lm1, "state")
+
+
+# barplots
+
+summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
+                      conf.interval=.95, .drop=TRUE) {
+  library(plyr)
+  
+  # New version of length which can handle NA's: if na.rm==T, don't count them
+  length2 <- function (x, na.rm=FALSE) {
+    if (na.rm) sum(!is.na(x))
+    else       length(x)
+  }
+  
+  # This does the summary. For each group's data frame, return a vector with
+  # N, mean, and sd
+  datac <- ddply(data, groupvars, .drop=.drop,
+                 .fun = function(xx, col) {
+                   c(N    = length2(xx[[col]], na.rm=na.rm),
+                     mean = mean   (xx[[col]], na.rm=na.rm),
+                     sd   = sd     (xx[[col]], na.rm=na.rm)
+                   )
+                 },
+                 measurevar
+  )
+  
+  # Rename the "mean" column    
+  datac <- rename(datac, c("mean" = measurevar))
+  
+  datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
+  
+  # Confidence interval multiplier for standard error
+  # Calculate t-statistic for confidence interval: 
+  # e.g., if conf.interval is .95, use .975 (above/below), and use df=N-1
+  ciMult <- qt(conf.interval/2 + .5, datac$N-1)
+  datac$ci <- datac$se * ciMult
+  
+  return(datac)
+}
+
+
+
+
+summary_biomass <- summarySE(biomass_for_anova2, measurevar="biomass", groupvars=c("state","yr"))
+
+ggplot(summary_biomass, aes(x=yr, y=biomass, fill=state)) + 
+  geom_bar(position=position_dodge(), stat="identity") +
+  geom_errorbar(aes(ymin=biomass-ci, ymax=biomass+ci),
+                width=.2,                    # Width of the error bars
+                position=position_dodge(.9))
+
+
+
+
+# bar chart phenology
+
+
+summary_phenology <- summarySE(all_but_photosynthesis, measurevar="duration_growth", groupvars=c("state"))
+
+ggplot(summary_phenology, aes(x=state, y=duration_growth, fill=state)) + 
+  geom_bar(position=position_dodge(), stat="identity") +
+  geom_errorbar(aes(ymin=duration_growth-ci, ymax=duration_growth+ci),
+                width=.2,                    # Width of the error bars
+                position=position_dodge(.9))
+
+
+
+# bar chart branch number
+
+
+summary_branch_num <- summarySE(all_but_photosynthesis, measurevar="june_branch_number", groupvars=c("state"))
+
+ggplot(summary_branch_num, aes(x=state, y=june_branch_number, fill=state)) + 
+  geom_bar(position=position_dodge(), stat="identity") +
+  geom_errorbar(aes(ymin=june_branch_number-ci, ymax=june_branch_number+ci),
+                width=.2,                    # Width of the error bars
+                position=position_dodge(.9))
+
+
+# bar chart leaf number
+
+
+summary_leaf_num <- summarySE(all_but_photosynthesis, measurevar="leaf_number", groupvars=c("state"))
+
+ggplot(summary_leaf_num, aes(x=state, y=leaf_number, fill=state)) + 
+  geom_bar(position=position_dodge(), stat="identity") +
+  geom_errorbar(aes(ymin=leaf_number-ci, ymax=leaf_number+ci),
+                width=.2,                    # Width of the error bars
+                position=position_dodge(.9))
+
+
+
+
+
 
 # separate anovas for the two years to test if split plot is giving same results
 # two ways to do this
